4d50a355f89a96d8c01a5adad57e7f16a59085ca
miaomiaokatze@gmail.com <miaomiaokatze@gmail.com@7c530c93-fbf8-ec1f-d5ab-0c51ff5d5b40>
2011-12-08 20:39:24 +0000

    greenpps源文件

---
+++ trunk/greenpps.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+REM == kill ppstream process ==
+TASKKILL /F /IM "PPStream.exe" /T
+REM == delete all pps ads directories ==
+RMDIR /s /q "%APPDATA%\PPStream\banner"
+ECHO banner >> "%APPDATA%\PPStream\banner"
+RMDIR /s /q "%APPDATA%\PPStream\adsys"
+ECHO adsys >> "%APPDATA%\PPStream\adsys"
+REM == detect ppstream director ==
+SET pps_dirs="%ProgramFiles%",C:\PPS.tv,D:\PPS.tv,E:\PPS.tv,C:\Programme,D:\Programme,E:\Programme,C:\Program Files,D:\Program Files,E:\Program Files
+FOR %%A IN (%pps_dirs%) DO (
+SET pps_cmd="%%~A\PPStream\PPStream.exe"
+IF EXIST "!pps_cmd!" (
+!pps_cmd! >> "%APPDATA%\PPStream\adsys"
+GOTO end
+)
+)
+:end
+ENDLOCAL
