93c7b88fcc620efa4041156dcde83598bc96d3e6
miaomiaokatze@gmail.com <miaomiaokatze@gmail.com@7c530c93-fbf8-ec1f-d5ab-0c51ff5d5b40>
2012-01-21 11:54:52 +0000

    fixed path bugs

--- trunk/greenpps.bat
+++ trunk/greenpps.bat
@@ -8,10 +8,12 @@ ECHO banner >> "%APPDATA%\PPStream\banner"
 RMDIR /s /q "%APPDATA%\PPStream\adsys"
 ECHO adsys >> "%APPDATA%\PPStream\adsys"
 REM == detect ppstream director ==
-SET pps_dirs="%ProgramFiles%",C:\PPS.tv,D:\PPS.tv,E:\PPS.tv,C:\Programme,D:\Programme,E:\Programme,C:\Program Files,D:\Program Files,E:\Program Files
+SET pps_dirs="%ProgramFiles%",C:\PPS.tv,D:\PPS.tv,E:\PPS.tv,C:\Programme,D:\Programme,E:\Programme,"C:\Program Files","D:\Program Files","E:\Program Files"
 FOR %%A IN (%pps_dirs%) DO (
 SET pps_cmd="%%~A\PPStream\PPStream.exe"
+REM echo "Check PPStream in !pps_cmd!"
 IF EXIST "!pps_cmd!" (
+echo "PPStream found in !pps_cmd!"
 !pps_cmd! >> "%APPDATA%\PPStream\adsys"
 GOTO end
 )
