3cccf2e5533973696ef0ce3062f526b8ef310aa9
miaomiaokatze@gmail.com <miaomiaokatze@gmail.com@7c530c93-fbf8-ec1f-d5ab-0c51ff5d5b40>
2012-05-10 20:02:25 +0000

    bugfixes

--- src/cn/kk/ppslauncher/RuntimeHelper.java
+++ src/cn/kk/ppslauncher/RuntimeHelper.java
@@ -1,4 +1,5 @@
 package cn.kk.ppslauncher;
+
 /*  Copyright (c) 2010 Xiaoyun Zhu
  * 
  *  Permission is hereby granted, free of charge, to any person obtaining a copy  
@@ -25,161 +26,174 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public final class RuntimeHelper {
-	private static final String[]	PATHS;
+    private static final String[] PATHS;
 
-	private static final String		OS_NAME	= System.getProperty("os.name").toLowerCase();
+    private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
 
-	private static final Runtime	RT			= Runtime.getRuntime();
+    private static final Runtime RT = Runtime.getRuntime();
 
-	public static boolean					SILENT;
+    public static boolean SILENT;
 
-	static {
-		String path = System.getenv("PATH");
-		if (path == null)
-			path = System.getenv("path");
-		if (path == null) {
-			PATHS = new String[0];
-		} else {
-			PATHS = path.split(File.pathSeparator);
-			int i = 0;
-			for (String p : PATHS) {
-				PATHS[i++] = p.trim() + File.separator;
-			}
-		}
-	}
+    static {
+        String path = System.getenv("PATH");
+        if (path == null)
+            path = System.getenv("path");
+        if (path == null) {
+            PATHS = new String[0];
+        } else {
+            PATHS = path.split(File.pathSeparator);
+            int i = 0;
+            for (String p : PATHS) {
+                PATHS[i++] = p.trim() + File.separator;
+            }
+        }
+    }
 
-	public static File findExecutable(final String executable, final String folder) {
-		File result = null;
-		if (null != (result = getExecutable(executable, folder))) {
-			return result;
-		}
-		for (String p : PATHS) {
-			if (null != (result = getExecutable(executable, p + folder))) {
-				return result;
-			}
-		}
-		if (result != (result = getExecutable(executable, System.getenv("ProgramFiles(x86)") + folder))) {
-			return result;
-		}
-		if (result != (result = getExecutable(executable, System.getenv("ProgramFiles") + folder))) {
-			return result;
-		}
-		if (result != (result = getExecutable(executable, System.getenv("ProgramW6432") + folder))) {
-			return result;
-		}
-		if (result != (result = getExecutable(executable, System.getenv("SystemDrive") + folder))) {
-			return result;
-		}
-		for (File f : File.listRoots()) {
-			final String root = f.getAbsolutePath();
-			String path = root.charAt(0) + System.getenv("ProgramFiles(x86)").substring(1);
-			if (null != (result = getExecutable(executable, path + File.separator + folder))) {
-				return result;
-			}
-			path = root.charAt(0) + System.getenv("ProgramFiles").substring(1);
-			if (null != (result = getExecutable(executable, path + File.separator + folder))) {
-				return result;
-			}
-			path = root.charAt(0) + System.getenv("ProgramW6432").substring(1);
-			if (null != (result = getExecutable(executable, path + File.separator + folder))) {
-				return result;
-			}
-			path = root + "Program Files";
-			if (null != (result = getExecutable(executable, path + File.separator + folder))) {
-				return result;
-			}
-			path = root + "Programme";
-			if (null != (result = getExecutable(executable, path + File.separator + folder))) {
-				return result;
-			}
-			if (null != (result = getExecutable(executable, root + File.separator + folder))) {
-				return result;
-			}
-		}
-		if (result == null && folder.length() != 0) {
-			return findExecutable(executable, "");
-		} else {
-			return result;
-		}
-	}
+    public static File findExecutable(final String executable, final String folder) {
+        File result = null;
+        if (null != (result = getExecutable(executable, folder))) {
+            return result;
+        }
+        for (String p : PATHS) {
+            if (null != (result = getExecutable(executable, p + folder))) {
+                return result;
+            }
+        }
+        final String pfDirX86 = System.getenv("ProgramFiles(x86)");
+        final String pfDir = System.getenv("ProgramFiles");
+        final String pfDirW = System.getenv("ProgramW6432");
+        if (pfDirX86 != null && result != (result = getExecutable(executable, pfDirX86 + folder))) {
+            return result;
+        }
+        if (pfDir != null && result != (result = getExecutable(executable, pfDir + folder))) {
+            return result;
+        }
+        if (pfDirW != null && result != (result = getExecutable(executable, pfDirW + folder))) {
+            return result;
+        }
+        if (result != (result = getExecutable(executable, System.getenv("SystemDrive") + folder))) {
+            return result;
+        }
+        for (File f : File.listRoots()) {
+            final String root = f.getAbsolutePath();
+            String path;
+            if (pfDirX86 != null) {
+                path = root.charAt(0) + pfDirX86.substring(1);
+                if (null != (result = getExecutable(executable, path + File.separator + folder))) {
+                    return result;
+                }
+            }
+            if (pfDir != null) {
+                path = root.charAt(0) + pfDir.substring(1);
+                if (null != (result = getExecutable(executable, path + File.separator + folder))) {
+                    return result;
+                }
+            }
+            if (pfDirW != null) {
+                path = root.charAt(0) + pfDirW.substring(1);
+                if (null != (result = getExecutable(executable, path + File.separator + folder))) {
+                    return result;
+                }
+            }
+            path = root + "Program Files";
+            if (null != (result = getExecutable(executable, path + File.separator + folder))) {
+                return result;
+            }
+            path = root + "Programme";
+            if (null != (result = getExecutable(executable, path + File.separator + folder))) {
+                return result;
+            }
+            if (null != (result = getExecutable(executable, root + File.separator + folder))) {
+                return result;
+            }
+        }
+        if (result == null && folder.length() != 0) {
+            return findExecutable(executable, "");
+        } else {
+            return result;
+        }
+    }
 
-	private static final File getExecutable(final String executable, final String path) {
-		File file = new File(path, executable);
-		if (file.isFile()) {
-			return file.getAbsoluteFile();
-		} else if ((file = new File(path, executable + ".exe")).isFile()) {
-			return file.getAbsoluteFile();
-		} else if ((file = new File(path, executable + ".cmd")).isFile()) {
-			return file.getAbsoluteFile();
-		} else if ((file = new File(path, executable + ".bat")).isFile()) {
-			return file.getAbsoluteFile();
-		}
-		return null;
-	}
+    private static final File getExecutable(final String executable, final String path) {
+        if (path == null) {
+            return null;
+        }
+        File file = new File(path, executable);
+        if (file.isFile()) {
+            return file.getAbsoluteFile();
+        } else if ((file = new File(path, executable + ".exe")).isFile()) {
+            return file.getAbsoluteFile();
+        } else if ((file = new File(path, executable + ".cmd")).isFile()) {
+            return file.getAbsoluteFile();
+        } else if ((file = new File(path, executable + ".bat")).isFile()) {
+            return file.getAbsoluteFile();
+        }
+        return null;
+    }
 
-	public static boolean isWindows() {
-		return OS_NAME.startsWith("windows");
-	}
+    public static boolean isWindows() {
+        return OS_NAME.startsWith("windows");
+    }
 
-	public static boolean isMac() {
-		return OS_NAME.startsWith("mac");
-	}
+    public static boolean isMac() {
+        return OS_NAME.startsWith("mac");
+    }
 
-	public static boolean isLinux() {
-		return OS_NAME.startsWith("linux");
-	}
+    public static boolean isLinux() {
+        return OS_NAME.startsWith("linux");
+    }
 
-	public static final int execAndWaitCommand(final String cmd) {
-		if (!SILENT) {
-			System.out.println(cmd);
-		}
-		try {
-			final Process proc = RT.exec(cmd);
-			if (!SILENT) {
-				new StreamRedirector(proc.getErrorStream(), System.err).start();
-				new StreamRedirector(proc.getInputStream(), System.out).start();
-				new StreamRedirector(System.in, proc.getOutputStream()).start();
-			} else {
-				new StreamRedirector(proc.getErrorStream(), null).start();
-				new StreamRedirector(proc.getInputStream(), null).start();
-			}
-			return proc.waitFor();
-		} catch (Exception e) {
-			if (!SILENT) {
-				System.err.println(e.toString());
-			}
-			return -1;
-		}
-	}
+    public static final int execAndWaitCommand(final String cmd) {
+        if (!SILENT) {
+            System.out.println(cmd);
+        }
+        try {
+            final Process proc = RT.exec(cmd);
+            if (!SILENT) {
+                new StreamRedirector(proc.getErrorStream(), System.err).start();
+                new StreamRedirector(proc.getInputStream(), System.out).start();
+                new StreamRedirector(System.in, proc.getOutputStream()).start();
+            } else {
+                new StreamRedirector(proc.getErrorStream(), null).start();
+                new StreamRedirector(proc.getInputStream(), null).start();
+            }
+            return proc.waitFor();
+        } catch (Exception e) {
+            if (!SILENT) {
+                System.err.println(e.toString());
+            }
+            return -1;
+        }
+    }
 
-	public static class StreamRedirector extends Thread {
-		private final InputStream		from;
+    public static class StreamRedirector extends Thread {
+        private final InputStream from;
 
-		private final OutputStream	to;
+        private final OutputStream to;
 
-		public StreamRedirector(InputStream from, OutputStream to) {
-			super();
-			this.from = from;
-			this.to = to;
-		}
+        public StreamRedirector(InputStream from, OutputStream to) {
+            super();
+            this.from = from;
+            this.to = to;
+        }
 
-		@Override
-		public void run() {
-			final byte[] buffer = new byte[1024];
-			int len;
-			try {
-				while ((len = from.read(buffer)) != -1) {
-					// System.out.println(new String(buffer, 0, len));
-					if (this.to != null) {
-						this.to.write(buffer, 0, len);
-						if (buffer[len - 1] == '\n') {
-							this.to.flush();
-						}
-					}
-				}
-			} catch (IOException e) {
-				// silent
-			}
-		}
-	}
+        @Override
+        public void run() {
+            final byte[] buffer = new byte[1024];
+            int len;
+            try {
+                while ((len = from.read(buffer)) != -1) {
+                    // System.out.println(new String(buffer, 0, len));
+                    if (this.to != null) {
+                        this.to.write(buffer, 0, len);
+                        if (buffer[len - 1] == '\n') {
+                            this.to.flush();
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                // silent
+            }
+        }
+    }
 }
